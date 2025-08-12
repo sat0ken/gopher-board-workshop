9eaaed69fd14f077cfe1c66a148e85f1dd9fdbe0
sat0ken <15720506+sat0ken@users.noreply.github.com>
2025-08-12 16:24:23 +0900

    update

---
+++ 03_pwm/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+var period uint64 = 2e9
+
+func main() {
+    pin := machine.D27
+	pwm := machine.PWM5
+
+	pwm.Configure(machine.PWMConfig{})
+
+	ch, err := pwm.Channel(pin)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	for {
+		// Fade the LED in.
+		for percentOn := 0; percentOn <= 100; percentOn++ {
+			pwm.Set(ch, pwm.Top()*uint32(percentOn)/100)
+			time.Sleep(time.Second / 100)
+		}
+
+		// Fade the LED out.
+		for percentOn := 100; percentOn >= 0; percentOn-- {
+			pwm.Set(ch, pwm.Top()*uint32(percentOn)/100)
+			time.Sleep(time.Second / 100)
+		}
+	}
+}
+
